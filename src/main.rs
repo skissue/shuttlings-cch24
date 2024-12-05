0f718b5d025f4ab0c088463c9c43838950b11e20
Ad <me@skissue.xyz>
2024-12-05 15:51:44 -0500

    feat: day -1 task 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use poem::{get, handler, Route};
+use poem::{
+    get, handler,
+    http::{header, StatusCode},
+    Response, Route,
+};
 use shuttle_poem::ShuttlePoem;
 
 #[handler]
@@ -6,9 +10,22 @@ fn hello_bird() -> &'static str {
     "Hello, bird!"
 }
 
+#[handler]
+fn seek() -> Response {
+    Response::builder()
+        .status(StatusCode::FOUND)
+        .header(
+            header::LOCATION,
+            "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        )
+        .body(())
+}
+
 #[shuttle_runtime::main]
 async fn poem() -> ShuttlePoem<impl poem::Endpoint> {
-    let app = Route::new().at("/", get(hello_bird));
+    let app = Route::new()
+        .at("/", get(hello_bird))
+        .at("/-1/seek", get(seek));
 
     Ok(app.into())
 }
