afc90c985a628a90d14c42af749f053c7556ea48
Ad <me@skissue.xyz>
2024-12-05 16:07:45 -0500

    feat: day 2 task 1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::net::Ipv4Addr;
+
 use poem::{
     get, handler,
     http::{header, StatusCode},
+    web::Query,
     Response, Route,
 };
+use serde::Deserialize;
 use shuttle_poem::ShuttlePoem;
 
 #[handler]
@@ -21,11 +25,33 @@ fn seek() -> Response {
         .body(())
 }
 
+#[derive(Deserialize)]
+struct EncryptParams {
+    from: Ipv4Addr,
+    key: Ipv4Addr,
+}
+
+#[handler]
+fn encrypt_address(params: Query<EncryptParams>) -> String {
+    let Query(EncryptParams { from, key }) = params;
+
+    let added: Vec<u8> = from
+        .octets()
+        .into_iter()
+        .zip(key.octets().into_iter())
+        .map(|(a, b)| a.wrapping_add(b))
+        .collect();
+    let dest = Ipv4Addr::new(added[0], added[1], added[2], added[3]);
+
+    dest.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn poem() -> ShuttlePoem<impl poem::Endpoint> {
     let app = Route::new()
         .at("/", get(hello_bird))
-        .at("/-1/seek", get(seek));
+        .at("/-1/seek", get(seek))
+        .at("/2/dest", get(encrypt_address));
 
     Ok(app.into())
 }
