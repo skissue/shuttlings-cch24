6a283698162de4bed56bedf75907d1cb15ebdc04
Ad <me@skissue.xyz>
2024-12-05 15:44:04 -0500

    feat: day -1 task 1

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use poem::{get, handler, Route};
 use shuttle_poem::ShuttlePoem;
 
 #[handler]
-fn hello_world() -> &'static str {
-    "Hello, world!"
+fn hello_bird() -> &'static str {
+    "Hello, bird!"
 }
 
 #[shuttle_runtime::main]
 async fn poem() -> ShuttlePoem<impl poem::Endpoint> {
-    let app = Route::new().at("/", get(hello_world));
+    let app = Route::new().at("/", get(hello_bird));
 
     Ok(app.into())
 }
