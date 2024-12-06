1696e34aa94f24cacabbe08170ca946868ab5554
Ad <me@skissue.xyz>
2024-12-05 20:17:09 -0500

    feat: day 2 task 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::Ipv4Addr;
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 use poem::{
     get, handler,
@@ -37,6 +37,18 @@ struct KeyParams {
     to: Ipv4Addr,
 }
 
+#[derive(Deserialize)]
+struct EncryptParamsV6 {
+    from: Ipv6Addr,
+    key: Ipv6Addr,
+}
+
+#[derive(Deserialize)]
+struct KeyParamsV6 {
+    from: Ipv6Addr,
+    to: Ipv6Addr,
+}
+
 #[handler]
 fn encrypt_address(params: Query<EncryptParams>) -> String {
     let Query(EncryptParams { from, key }) = params;
@@ -67,13 +79,57 @@ fn get_address_key(params: Query<KeyParams>) -> String {
     key.to_string()
 }
 
+#[handler]
+fn encrypt_address_ipv6(params: Query<EncryptParamsV6>) -> String {
+    let Query(EncryptParamsV6 { from, key }) = params;
+
+    let xored: Vec<u8> = from
+        .octets()
+        .into_iter()
+        .zip(key.octets().into_iter())
+        .map(|(a, b)| a ^ b)
+        .collect();
+
+    let mut octets: [u8; 16] = [0; 16];
+    for i in 0..16 {
+        octets[i] = xored[i];
+    }
+
+    let to = Ipv6Addr::from(octets);
+
+    to.to_string()
+}
+
+#[handler]
+fn get_address_key_ipv6(params: Query<KeyParamsV6>) -> String {
+    let Query(KeyParamsV6 { from, to }) = params;
+
+    let xored: Vec<u8> = from
+        .octets()
+        .into_iter()
+        .zip(to.octets().into_iter())
+        .map(|(a, b)| a ^ b)
+        .collect();
+
+    let mut octets: [u8; 16] = [0; 16];
+    for i in 0..16 {
+        octets[i] = xored[i];
+    }
+
+    let key = Ipv6Addr::from(octets);
+
+    key.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn poem() -> ShuttlePoem<impl poem::Endpoint> {
     let app = Route::new()
         .at("/", get(hello_bird))
         .at("/-1/seek", get(seek))
         .at("/2/dest", get(encrypt_address))
-        .at("/2/key", get(get_address_key));
+        .at("/2/key", get(get_address_key))
+        .at("/2/v6/dest", get(encrypt_address_ipv6))
+        .at("/2/v6/key", get(get_address_key_ipv6));
 
     Ok(app.into())
 }
