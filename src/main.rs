393927a38028ba9c466a07b5606a457264d9e276
Ad <me@skissue.xyz>
2024-12-10 15:03:55 -0500

    feat: day 9 task 2-4
    Forgot to commit individual tasks 😔. Also, solution for task 4 is horrendous,
    but it works, so ¯\_(ツ)_/¯.

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,23 @@ use cargo_manifest::Manifest;
 use itertools::Itertools;
 use poem::{
     get, handler,
-    http::{header, StatusCode},
+    http::{header, HeaderMap, StatusCode},
     post,
-    web::{Data, Query},
+    web::{
+        headers::{ContentType, Header},
+        Data, Query, TypedHeader,
+    },
     EndpointExt, Response, Route,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use shuttle_poem::ShuttlePoem;
 use std::{
+    cell::RefCell,
     net::{Ipv4Addr, Ipv6Addr},
-    sync::Arc,
+    sync::{Arc, RwLock},
     time::Duration,
 };
+use tokio::sync::Mutex;
 use toml::Table;
 
 #[handler]
@@ -174,17 +179,64 @@ fn order_manifests(body: Vec<u8>) -> Response {
 }
 
 #[derive(Clone)]
-struct MilkBucket(Arc<leaky_bucket::RateLimiter>);
+struct MilkBucket(Arc<Mutex<leaky_bucket::RateLimiter>>);
+
+#[derive(Serialize, Deserialize)]
+#[serde(untagged, deny_unknown_fields)]
+enum MilkConversion {
+    Liters { liters: f32 },
+    Gallons { gallons: f32 },
+    IHateTheBritish { litres: f32 },
+    IHateTheBritishMore { pints: f32 },
+}
 
 #[handler]
-async fn leaky_milk(bucket: Data<&MilkBucket>) -> Response {
-    if !bucket.0 .0.try_acquire(1) {
+async fn leaky_milk(headers: &HeaderMap, bucket: Data<&MilkBucket>, body: String) -> Response {
+    if !bucket.0 .0.lock().await.try_acquire(1) {
         return Response::builder()
             .status(StatusCode::TOO_MANY_REQUESTS)
             .body("No milk available\n");
     }
 
+    if headers
+        .get("Content-Type")
+        .map_or(false, |v| v == "application/json")
+    {
+        let Ok(conversion_request): Result<MilkConversion, _> = serde_json::from_str(&body) else {
+            return StatusCode::BAD_REQUEST.into();
+        };
+
+        let response = match conversion_request {
+            MilkConversion::Liters { liters } => {
+                let gallons = liters * 0.2641720524;
+                MilkConversion::Gallons { gallons }
+            }
+            MilkConversion::Gallons { gallons } => {
+                let liters = gallons * 3.785411784;
+                MilkConversion::Liters { liters }
+            }
+            MilkConversion::IHateTheBritish { litres } => {
+                let pints = litres * 1.75975;
+                MilkConversion::IHateTheBritishMore { pints }
+            }
+            MilkConversion::IHateTheBritishMore { pints } => {
+                let litres = pints * 0.56826;
+                MilkConversion::IHateTheBritish { litres }
+            }
+        };
+        return serde_json::to_string(&response).unwrap().into();
+    }
+
     "Milk withdrawn\n".into()
+}
+
+#[handler]
+async fn fill_milk_bucket(bucket: Data<&MilkBucket>) {
+    *bucket.0 .0.lock().await = leaky_bucket::RateLimiter::builder()
+        .initial(5)
+        .max(5)
+        .interval(Duration::from_secs(1))
+        .build();
 }
 
 #[shuttle_runtime::main]
@@ -198,12 +250,14 @@ async fn poem() -> ShuttlePoem<impl poem::Endpoint> {
         .at("/2/v6/key", get(get_address_key_ipv6))
         .at("/5/manifest", post(order_manifests))
         .at("/9/milk", post(leaky_milk))
-        .data(MilkBucket(Arc::new(
+        .at("/9/refill", post(fill_milk_bucket))
+        .data(MilkBucket(Arc::new(Mutex::new(
             leaky_bucket::RateLimiter::builder()
+                .initial(5)
                 .max(5)
                 .interval(Duration::from_secs(1))
                 .build(),
-        )));
+        ))));
 
     Ok(app.into())
 }
