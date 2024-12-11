67254a83ba3fba309fe9002738d5c5ceac99fd5e
Ad <me@skissue.xyz>
2024-12-11 08:33:01 -0500

    feat(nix): switch to nixpkgs-unstable
    Shuttle PR got merged 🎉.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
-    # Newer Shuttle CLI version; https://github.com/NixOS/nixpkgs/pull/361827
-    nixpkgs.url = "github:nixos/nixpkgs/9f609517c8343ffb711ebe928805e8fbfcc1ecc7";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {nixpkgs, ...} @ inputs: let
