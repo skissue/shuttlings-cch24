57640c69bd9b08db7a48c1a5934ce599329cfc44
Ad <me@skissue.xyz>
2024-12-23 09:17:30 -0500

    feat: properly handle error for task 3

--- src/day23.rs
+++ src/day23.rs
@@ -1,3 +1,4 @@
+use http::StatusCode;
 use poem::{endpoint::StaticFilesEndpoint, web::Path, *};
 
 pub fn route() -> Route {
@@ -13,14 +14,14 @@ async fn light_star() -> impl IntoResponse {
 }
 
 #[handler]
-async fn cycle_present_color(color: Path<String>) -> impl IntoResponse {
+async fn cycle_present_color(color: Path<String>) -> Result<impl IntoResponse> {
     let next = match (*color).as_str() {
         "red" => "blue",
         "blue" => "purple",
         "purple" => "red",
-        _ => unreachable!()
+        _ => return Err(StatusCode::IM_A_TEAPOT.into()),
     };
-    format!(
+    Ok(format!(
         r#"
           <div class="present {}" hx-get="/23/present/{}" hx-swap="outerHTML">
               <div class="ribbon"></div>
@@ -30,5 +31,5 @@ async fn cycle_present_color(color: Path<String>) -> impl IntoResponse {
           </div>
         "#,
         *color, next
-    )
+    ))
 }
