758dfe6e87849783c68a7fb8246775e6f589c8df
Ad <me@skissue.xyz>
2024-12-23 09:06:43 -0500

    feat: day 23 task 2

--- src/day23.rs
+++ src/day23.rs
@@ -1,5 +1,12 @@
 use poem::{endpoint::StaticFilesEndpoint, *};
 
 pub fn route() -> Route {
-    Route::new().nest("/assets", StaticFilesEndpoint::new("assets"))
+    Route::new()
+        .nest("/assets", StaticFilesEndpoint::new("assets"))
+        .at("/23/star", get(light_star))
+}
+
+#[handler]
+async fn light_star() -> impl IntoResponse {
+    r#"<div id="star" class="lit"></div>"#
 }
